feea40bacadeee674549db3dea04a2fc619cb7b4
lukwac123 <lukasz-waclawek@wp.pl>
2024-11-11 11:43:38 +0100

    Dodanie modeli katalogu w witrynie administracyjnej

--- myshop/settings.py
+++ myshop/settings.py
@@ -31,13 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Moje aplikacje
+    'shop.apps.ShopConfig',
+
+    # Pozostałe aplikacje
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig',
+
 ]
 
 MIDDLEWARE = [
