33e09241d6294c01b208ef7d139dad28c139560f
lukwac123 <lukasz-waclawek@wp.pl>
2024-11-14 13:47:52 +0100

    Utworzenie systemu zamówień klienta

--- orders/models.py
+++ orders/models.py
@@ -25,6 +25,7 @@ class Order(models.Model):
         return sum(item.get_cost() for item in self.items.all())
 
 class OrderItem(models.Model):
+    objects = models.Manager()
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='order_item', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
