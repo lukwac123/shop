42dd9084ccabbcbe886e738ce588e7fcdf2d8608
lukwac123 <lukasz-waclawek@wp.pl>
2024-11-19 12:51:34 +0100

    Poprawienie settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import SESSION_EXPIRE_AT_BROWSER_CLOSE, EMAIL_BACKEND
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
