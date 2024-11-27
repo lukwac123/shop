2fca2ba8be9d4645d93ca489c482319e6db81716
lukwac123 <lukasz-waclawek@wp.pl>
2024-11-27 15:59:24 +0100

    Zastosowanie kuponu w zamówieniu.

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from shop.models import Product
 from django.conf import settings
-
-# Create your models here.
+from decimal import Decimal
+from django.core.validators import MinValueValidator, MaxValueValidator
+from coupons.models import Coupon
 
 class Order(models.Model):
     objects = models.Manager()
@@ -16,6 +17,8 @@ class Order(models.Model):
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
     stripe_id = models.CharField(max_length=250, blank=True)
+    coupon = models.ForeignKey(Coupon, related_name='orders', null=True, blank=True, on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
     class Meta:
         ordering = ['-created']
@@ -25,7 +28,8 @@ class Order(models.Model):
         return f'Zamówienie {self.id}'
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
     
     def get_stripe_url(self):
         if not self.stripe_id:
@@ -38,6 +42,15 @@ class Order(models.Model):
             # Ścieżka dla rzeczywistych płatności Stripe
             path = '/'
         return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+    
+    def get_total_cost_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
 
 class OrderItem(models.Model):
     objects = models.Manager()
