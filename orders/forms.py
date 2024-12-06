1bc54f6df9decdca4ff637ef6b74e0967c321a70
lukwac123 <lukasz-waclawek@wp.pl>
2024-12-06 14:25:41 +0100

    Adaptacja widoków dla tłumaczeń.

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,9 @@
 from django import forms
+from localflavor.pl.forms import PLPostalCodeField
 from .models import Order
 
 class OrderCreateForm(forms.ModelForm):
+    postal_code = PLPostalCodeField()
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
